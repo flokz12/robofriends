d1696f0de6f5f1773669116f5b0848039fb63b7f
Jeffrey Papa <jfpapa@cvsu-naic.edu.ph>
2023-08-26 00:12:28 +0800

    edited

--- src/containers/App.js
+++ src/containers/App.js
@@ -35,7 +35,7 @@ render() {
     } else {
         return (
             <div className='tc'>
-                <h1 className = 'f1'>RoboFriends</h1>
+                <h1 className = 'f1'>RoboFriends!</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
